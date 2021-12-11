42d893739b25641a08f80e47f9fc64d9a6750234
Jwiggiff <friedman.josh03@gmail.com>
2021-12-11 02:03:47 -0500

    add newlines

--- templates/content/download.php
+++ templates/content/download.php
@@ -2,8 +2,11 @@
 use OCA\URLDownloader\Services\Downloader;
 
 echo "test";
+
 echo $_SERVER['DOCUMENT_ROOT'];
+
 echo __DIR__;
+
 echo getcwd();
 
 $d = new Downloader("url", "/home/josh/test.txt");
